9c213edd111d487c47c26b23a5e221e8d6680e64
Hassan McGinnis <hassan.mcginnis@gmail.com>
2017-08-02 11:13:24 -0400

    Added rudimentary and untested install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p ~/.cfgbackup
+
+for file in files/.[^.]*; do
+    shortfile=${file/files\/}
+    cp ~/$shortfile .cfgbackup/ -v
+    #echo $shortfile
+done
+
+printf '\n\nExisting dotfiles backed up to ~/.cfgbackup folder.\n\n'
+
+for file in files/.[^.]*; do
+    shortfile=${file/files\/}
+    ln -s $file ~/$shortfile
+done
+
+printf '\n\nSymlinks created for dotfiles from git repo.\n\n'
