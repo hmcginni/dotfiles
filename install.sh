c4fc8f57f20f5f103804b2d9328f4816b76169f5
Hassan McGinnis <hassan.mcginnis@gmail.com>
2017-08-03 11:36:16 -0400

    added some packages to install

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Install packages #
+####################
+
+sudo apt update && sudo apt upgrade && sudo apt autoremove && sudo apt autoclean
+sudo apt install tmux stterm rxvt-unicode build-essential emacs emacs-goodies-el
+
+
+# Copy dots to ~ #
+##################
+
 mkdir -p ~/.dotbackup
 
 for file in files/.[^.]*; do
@@ -12,8 +22,8 @@ printf '\n\nExisting dotfiles backed up to ~/.dotbackup folder.\n\n'
 
 for file in files/.[^.]*; do
     shortfile=${file/files\/}
-    ln -s "`pwd`/$file" ~/$shortfile
-    echo "`pwd`/$file"
+    ln -sv "`pwd`/$file" ~/$shortfile
 done
 
 printf '\n\nSymlinks created for dotfiles from git repo.\n\n'
+
